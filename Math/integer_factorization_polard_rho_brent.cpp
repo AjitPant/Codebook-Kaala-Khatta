d6cd9e777a8dbc826c34eeb01f275fb0149b9777
Ajit Pant <43715439+AjitPant@users.noreply.github.com>
2022-09-24 14:08:29 +0530

    Create integer_factorization_polard_rho_brent.cpp

---
+++ Math/integer_factorization_polard_rho_brent.cpp
@@ -0,0 +1,41 @@
+long long mult(long long a, long long b, long long mod) {
+    return (__int128)a * b % mod;
+}
+
+long long f(long long x, long long c, long long mod) {
+    return (mult(x, x, mod) + c) % mod;
+}
+
+
+long long brent(long long n, long long x0=2, long long c=1) {
+    long long x = x0;
+    long long g = 1;
+    long long q = 1;
+    long long xs, y;
+
+    int m = 128;
+    int l = 1;
+    while (g == 1) {
+        y = x;
+        for (int i = 1; i < l; i++)
+            x = f(x, c, n);
+        int k = 0;
+        while (k < l && g == 1) {
+            xs = x;
+            for (int i = 0; i < m && i < l - k; i++) {
+                x = f(x, c, n);
+                q = mult(q, abs(y - x), n);
+            }
+            g = gcd(q, n);
+            k += m;
+        }
+        l *= 2;
+    }
+    if (g == n) {
+        do {
+            xs = f(xs, c, n);
+            g = gcd(abs(xs - y), n);
+        } while (g == 1);
+    }
+    return g;
+}
