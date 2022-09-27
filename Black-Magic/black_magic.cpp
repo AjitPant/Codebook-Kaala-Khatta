cd9da28f758855b561c41b4f7d329d27c8bf57fd
Ajit Pant <43715439+AjitPant@users.noreply.github.com>
2022-09-28 01:37:45 +0530

    Update black_magic.cpp

--- Black-Magic/black_magic.cpp
+++ Black-Magic/black_magic.cpp
@@ -1,3 +1,6 @@
+#pragma GCC optimize ("O3,unroll-loops,no-stack-protector")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
 #include <bits/extc++.h>
 using namespace __gnu_pbds;
 typedef tree<int,null_type,less<int>,rb_tree_tag,
