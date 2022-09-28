af036cdc4dcd670edd935bfc1f788800a50bac15
Ajit Pant <43715439+AjitPant@users.noreply.github.com>
2022-09-28 13:46:56 +0530

    Update Fenwick.cpp

--- range_query/Fenwick.cpp
+++ range_query/Fenwick.cpp
@@ -1,9 +1,6 @@
 struct FenwickTree2D {
     vector<vector<int>> bit;
     int n, m;
-
-    // init(...) { ... }
-
     int sum(int x, int y) {
         int ret = 0;
         for (int i = x; i >= 0; i = (i & (i + 1)) - 1)
@@ -11,7 +8,6 @@ struct FenwickTree2D {
                 ret += bit[i][j];
         return ret;
     }
-
     void add(int x, int y, int delta) {
         for (int i = x; i < n; i = i | (i + 1))
             for (int j = y; j < m; j = j | (j + 1))
