e4aedbe21b98c99779b2cae413fad05fb64474ac
Ajit Pant <43715439+AjitPant@users.noreply.github.com>
2022-09-28 14:04:29 +0530

    Update Gray_code.cpp

--- Math/Gray_code.cpp
+++ Math/Gray_code.cpp
@@ -1,10 +1,6 @@
-int g(int n) {
-    return n ^ (n >> 1);
-}
-
+int g(int n) {return n ^ (n >> 1);}
 int rev_g(int g) {
   int n = 0;
-  for (; g; g >>= 1)
-    n ^= g;
+  for (; g; g >>= 1) n ^= g;
   return n;
 }
