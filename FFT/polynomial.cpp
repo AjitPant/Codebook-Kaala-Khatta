e2023c77f4fbbeadae197a33d1ab0c3a2ad012c3
Ajit Pant <43715439+AjitPant@users.noreply.github.com>
2022-09-28 18:16:25 +0530

    Update polynomial.cpp

--- FFT/polynomial.cpp
+++ FFT/polynomial.cpp
@@ -7,67 +7,10 @@ namespace algebra {
         typedef double ftype;
         typedef complex<ftype> point;
 
-        vector<point> w;
         const ftype pi = acos(-1);
-        bool initiated = 0;
-        void init() {
-            if(!initiated) {
-                w.resize(maxn);
-                for(int i = 1; i < maxn; i *= 2) {
-                    for(int j = 0; j < i; j++) {
-                        w[i + j] = polar(ftype(1),2* pi * j / i);
-                    }
-                }
-                initiated = 1;
-            }
-        }
-        template<typename T>
-        void fft(T *in, point *out, int n) {
-
-            for (int i = 1, j = 0; i < n; i++) {
-                int bit = n >> 1;
-                for (; j & bit; bit >>= 1)
-                    j ^= bit;
-                j ^= bit;
-
-                if (i < j)
-                    swap(in[i], in[j]);
-            }
-
-            for (int len = 2; len <= n; len <<= 1) {
-                for (int i = 0; i < n; i += len) {
-                    for (int j = 0; j < len / 2; j++) {
-                        point u = in[i+j], v = in[i+j+len/2] * w[j  + len];
-                        in[i+j] = u + v;
-                        in[i+j+len/2] = u - v;
-                    }
-                }
-            }
-
-            for(int i= 0;i<n;i++)
-                out[i] = in[i];
-
-        }
-
-        template<typename T>
-        void mul_slow(vector<T> &a, const vector<T> &b) {
-            vector<T> res(a.size() + b.size() - 1);
-            for(size_t i = 0; i < a.size(); i++) {
-                for(size_t j = 0; j < b.size(); j++) {
-                    res[i + j] += a[i] * b[j];
-                }
-            }
-            a = res;
-        }
-
 
         template<typename T>
         void mul(vector<T> &a, const vector<T> &b) {
-            if(min(a.size(), b.size()) < magic) {
-                mul_slow(a, b);
-                return;
-            }
-            init();
             static const int shift = 15, mask = (1 << shift) - 1;
             size_t n = a.size() + b.size() - 1;
             while(__builtin_popcount(n) != 1) {
@@ -107,54 +50,6 @@ namespace algebra {
         }
     }
     template<typename T>
-    T bpow(T x, size_t n) {
-        return n ? n % 2 ? x * bpow(x, n - 1) : bpow(x * x, n / 2) : T(1);
-    }
-    template<typename T>
-    T bpow(T x, size_t n, T m) {
-        return n ? n % 2 ? x * bpow(x, n - 1, m) % m : bpow(x * x % m, n / 2, m) : T(1);
-    }
-    template<typename T>
-    T gcd(const T &a, const T &b) {
-        return b == T(0) ? a : gcd(b, a % b);
-    }
-    template<typename T>
-    T nCr(T n, int r) { // runs in O(r)
-        T res(1);
-        for(int i = 0; i < r; i++) {
-            res *= (n - T(i));
-            res /= (i + 1);
-        }
-        return res;
-    }
-
-    template<int m>
-    struct modular {
-        int64_t r;
-        modular() : r(0) {}
-        modular(int64_t rr) : r(rr) {if(abs(r) >= m) r %= m; if(r < 0) r += m;}
-        modular inv() const {return bpow(*this, m - 2);}
-        modular operator * (const modular &t) const {return (r * t.r) % m;}
-        modular operator / (const modular &t) const {return *this * t.inv();}
-        modular operator += (const modular &t) {r += t.r; if(r >= m) r -= m; return *this;}
-        modular operator -= (const modular &t) {r -= t.r; if(r < 0) r += m; return *this;}
-        modular operator + (const modular &t) const {return modular(*this) += t;}
-        modular operator - (const modular &t) const {return modular(*this) -= t;}
-        modular operator *= (const modular &t) {return *this = *this * t;}
-        modular operator /= (const modular &t) {return *this = *this / t;}
-
-        bool operator == (const modular &t) const {return r == t.r;}
-        bool operator != (const modular &t) const {return r != t.r;}
-
-        operator int64_t() const {return r;}
-    };
-    template<int T>
-    istream& operator >> (istream &in, modular<T> &x) {
-        return in >> x.r;
-    }
-
-
-    template<typename T>
     struct poly {
         vector<T> a;
 
@@ -168,43 +63,6 @@ namespace algebra {
         poly(T a0) : a{a0}{normalize();}
         poly(vector<T> t) : a(t){normalize();}
 
-        poly operator += (const poly &t) {
-            a.resize(max(a.size(), t.a.size()));
-            for(size_t i = 0; i < t.a.size(); i++) {
-                a[i] += t.a[i];
-            }
-            normalize();
-            return *this;
-        }
-        poly operator -= (const poly &t) {
-            a.resize(max(a.size(), t.a.size()));
-            for(size_t i = 0; i < t.a.size(); i++) {
-                a[i] -= t.a[i];
-            }
-            normalize();
-            return *this;
-        }
-        poly operator + (const poly &t) const {return poly(*this) += t;}
-        poly operator - (const poly &t) const {return poly(*this) -= t;}
-
-        poly mod_xk(size_t k) const { // get same polynomial mod x^k
-            k = min(k, a.size());
-            return vector<T>(begin(a), begin(a) + k);
-        }
-        poly mul_xk(size_t k) const { // multiply by x^k
-            poly res(*this);
-            res.a.insert(begin(res.a), k, 0);
-            return res;
-        }
-        poly div_xk(size_t k) const { // divide by x^k, dropping coefficients
-            k = min(k, a.size());
-            return vector<T>(begin(a) + k, end(a));
-        }
-        poly substr(size_t l, size_t r) const { // return mod_xk(r).div_xk(l)
-            l = min(l, a.size());
-            r = min(r, a.size());
-            return vector<T>(begin(a) + l, begin(a) + r);
-        }
         poly inv(size_t n) const { // get inverse series mod x^n
             assert(!is_zero());
             poly ans = a[0].inv();
@@ -216,19 +74,6 @@ namespace algebra {
             }
             return ans.mod_xk(n);
         }
-
-        poly operator *= (const poly &t) {fft::mul(a, t.a); normalize(); return *this;}
-        poly operator * (const poly &t) const {return poly(*this) *= t;}
-
-        poly reverse(size_t n, bool rev = 0) const { // reverses and leaves only n terms
-            poly res(*this);
-            if(rev) { // If rev = 1 then tail goes to head
-                res.a.resize(max(n, res.a.size()));
-            }
-            std::reverse(res.a.begin(), res.a.end());
-            return res.mod_xk(n);
-        }
-
         pair<poly, poly> divmod_slow(const poly &b) const { // when divisor or quotient is small
             vector<T> A(a);
             vector<T> res;
@@ -257,33 +102,6 @@ namespace algebra {
             return {D, *this - D * b};
         }
 
-        poly operator / (const poly &t) const {return divmod(t).first;}
-        poly operator % (const poly &t) const {return divmod(t).second;}
-        poly operator /= (const poly &t) {return *this = divmod(t).first;}
-        poly operator %= (const poly &t) {return *this = divmod(t).second;}
-        poly operator *= (const T &x) {
-            for(auto &it: a) {
-                it *= x;
-            }
-            normalize();
-            return *this;
-        }
-        poly operator /= (const T &x) {
-            for(auto &it: a) {
-                it /= x;
-            }
-            normalize();
-            return *this;
-        }
-        poly operator * (const T &x) const {return poly(*this) *= x;}
-        poly operator / (const T &x) const {return poly(*this) /= x;}
-
-        void print() const {
-            for(auto it: a) {
-                cout << it << ' ';
-            }
-            cout << endl;
-        }
         T eval(T x) const { // evaluates in single point x
             T res(0);
             for(int i = int(a.size()) - 1; i >= 0; i--) {
@@ -515,7 +333,6 @@ namespace algebra {
 
 using namespace algebra;
 
-typedef modular<mod> base;
 typedef poly<base> polyn;
 
 using namespace algebra;
