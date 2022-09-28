fc6bd27ee7f80d31d62991fd5781e1c0377b5d8d
Ajit Pant <43715439+AjitPant@users.noreply.github.com>
2022-09-28 18:40:51 +0530

    Update and rename gauss_any_mod.cpp to gauss any mod.cpp

--- Matrix/gauss any mod.cpp
+++ Matrix/gauss any mod.cpp
@@ -1,4 +1,3 @@
-
 int gauss (vector < vector<int> > &a, vector<int> & ans) {
     int n = (int) a.size();
     int m = (int) a[0].size() - 1;
@@ -14,7 +13,6 @@ int gauss (vector < vector<int> > &a, vector<int> & ans) {
         for (int i=col; i<=m; ++i)
             swap (a[sel][i], a[row][i]);
         where[col] = row;
-
         for (int i=0; i<n; ++i)
             if (i != row) {
                 int c = a[i][col] *mod_inv( a[row][col], mod)%mod;
@@ -26,22 +24,18 @@ int gauss (vector < vector<int> > &a, vector<int> & ans) {
             }
         ++row;
     }
-
     ans.assign (m, 0);
     vi out(1);
     for (int i=0; i<m; ++i)
         if (where[i] != -1)
             ans[i] = a[where[i]][m] * mod_inv( a[where[i]][i], mod)%mod;
-
     for (int i=0; i<n; ++i) {
         int sum = 0;
         for (int j=0; j<m; ++j)
             sum =(sum +  ans[j] * a[i][j])%mod;
-
         if (sum != a[i][m])
             return -1;
     }
-
     for (int i=0; i<m; ++i)
         if (where[i] == -1)
             return 2;
