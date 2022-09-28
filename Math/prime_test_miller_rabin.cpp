8b9ed3cf81d7517d25e2121cfdca02e1690737d0
Ajit Pant <43715439+AjitPant@users.noreply.github.com>
2022-09-28 14:06:08 +0530

    Update prime_test_miller_rabin.cpp

--- Math/prime_test_miller_rabin.cpp
+++ Math/prime_test_miller_rabin.cpp
@@ -1,18 +1,6 @@
 using u64 = uint64_t;
 using u128 = __uint128_t;
 
-u64 binpower(u64 base, u64 e, u64 mod) {
-    u64 result = 1;
-    base %= mod;
-    while (e) {
-        if (e & 1)
-            result = (u128)result * base % mod;
-        base = (u128)base * base % mod;
-        e >>= 1;
-    }
-    return result;
-}
-
 bool check_composite(u64 n, u64 a, u64 d, int s) {
     u64 x = binpower(a, d, n);
     if (x == 1 || x == n - 1)
