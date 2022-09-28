1d18597712e3458c8c58fc60ae423b3e2fa5b40a
Ajit Pant <43715439+AjitPant@users.noreply.github.com>
2022-09-28 14:04:49 +0530

    Update baby_step_giant_step.cpp

--- Math/baby_step_giant_step.cpp
+++ Math/baby_step_giant_step.cpp
@@ -2,17 +2,14 @@
 int solve(int a, int b, int m) {
     a %= m, b %= m;
     int n = sqrt(m) + 1;
-
     int an = 1;
     for (int i = 0; i < n; ++i)
         an = (an * 1ll * a) % m;
-
     unordered_map<int, int> vals;
     for (int q = 0, cur = b; q <= n; ++q) {
         vals[cur] = q;
         cur = (cur * 1ll * a) % m;
     }
-
     for (int p = 1, cur = 1; p <= n; ++p) {
         cur = (cur * 1ll * an) % m;
         if (vals.count(cur)) {
