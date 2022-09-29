6e0b112df8f31233669d30d2fe9a3f6b2ea16484
Ajit Pant <43715439+AjitPant@users.noreply.github.com>
2022-09-30 01:04:27 +0530

    Update Black Magic.cpp

--- Black-Magic/Black Magic.cpp
+++ Black-Magic/Black Magic.cpp
@@ -13,21 +13,15 @@ typedef priority_queue<int> heap;
 #include <ext/rope>
 using namespace __gnu_cxx;
 int main() {
-  // Insert some entries into s.
   set_t s;
   s.insert(12);
   s.insert(505);
-  // The order of the keys should be: 12, 505.
   assert(*s.find_by_order(0) == 12);
   assert(*s.find_by_order(3) == 505);
-  // The order of the keys should be: 12, 505.
   assert(s.order_of_key(12) == 0);
   assert(s.order_of_key(505) == 1);
-  // Erase an entry.
   s.erase(12);
-  // The order of the keys should be: 505.
   assert(*s.find_by_order(0) == 505);
-  // The order of the keys should be: 505.
   assert(s.order_of_key(505) == 0);
   heap h1, h2;
   h1.join(h2);
