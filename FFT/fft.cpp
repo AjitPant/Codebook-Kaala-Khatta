217fabe94f02e550429fb17d9da4c5c38ff2d938
Ajit Pant <43715439+AjitPant@users.noreply.github.com>
2022-09-30 17:57:46 +0530

    Update fft.cpp

--- FFT/fft.cpp
+++ FFT/fft.cpp
@@ -1,14 +1,11 @@
 using cd = complex<double>;
 const double PI = acos(-1);
-
 void fft(vector<cd> &a, bool invert) {
   int n = a.size();
-
   for (int i = 1, j = 0; i < n; i++) {
     int bit = n >> 1;
     for (; j & bit; bit >>= 1) j ^= bit;
     j ^= bit;
-
     if (i < j) swap(a[i], a[j]);
   }
 
@@ -25,7 +22,6 @@ void fft(vector<cd> &a, bool invert) {
       }
     }
   }
-
   if (invert) {
     for (cd &x : a) x /= n;
   }
@@ -37,13 +33,10 @@ vector<int> multiply(vector<int> const &a, vector<int> const &b) {
   while (n < a.size() + b.size()) n <<= 1;
   fa.resize(n);
   fb.resize(n);
-
   fft(fa, false);
   fft(fb, false);
   for (int i = 0; i < n; i++) fa[i] *= fb[i];
   fft(fa, true);
-
   vector<int> result(n);
   for (int i = 0; i < n; i++) result[i] = round(fa[i].real());
-  return result;
-}
+  return result;}
